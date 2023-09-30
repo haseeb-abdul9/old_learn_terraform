bf1a2fe01e51eeb24172b6b071a8129fa1acb369
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-09-30 01:54:00 -0400

    terraform data block intro

--- sample.tf
+++ sample.tf
@@ -8,9 +8,9 @@
 }*/
 
 data "aws_ami" "centos" {
-  owners           = ["973714476881"]
+  owners           = [973714476881]
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
+  name_regex       = Centos-8-DevOps-Practice
 }"
 
 output "ami" {
