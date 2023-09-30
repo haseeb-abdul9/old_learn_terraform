10dc6dffb37430b6cf136fdb1b0b3f0ca68bc894
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-09-30 01:52:16 -0400

    terraform data block intro

--- sample.tf
+++ sample.tf
@@ -1,8 +1,18 @@
-resource "aws_instance" "web" {
+/*resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}*/
+
+data "aws_ami" "centos" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}"
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
 }
