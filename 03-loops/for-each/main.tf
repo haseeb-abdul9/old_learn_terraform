cd8e9f12c5e66806ac02c52ecdd157ce274028ee
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-10-06 08:13:39 -0400

    for each learning

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,39 @@
+resource "null_resource" "null" {
+  count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo fruit name - ${var.fruits[count.index]}"
+  }
+}
+
+
+variable "fruits" {
+  default = [ "apple", "banana", "mango" ]
+}
+
+resource "null_resource" "null" {
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo fruit name - ${each.key} - ${each.value["count"]}"
+  }
+}
+
+
+
+variable "fruits1" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
+    banana = {
+      name = "banana"
+      count = 20
+    }
+    mango = {
+      name = "mango"
+      count = 30
+    }
+  }
+}
