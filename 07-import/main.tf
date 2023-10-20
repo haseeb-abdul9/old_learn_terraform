b8e16255f2f4c249d679418bdd52cf2769d79f65
mohammad9haseeb <mohammad9haseeb@gmail.com>
2023-10-20 04:07:04 -0400

    import

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -1,5 +1,8 @@
-resource "aws_instance" "workstation" {}
+resource "aws_instance" "workstation" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
 
-output "private_ip" {
-  value = aws_instance.workstation.private_ip
+  tags = {
+    name = "workstation"
+  }
 }
